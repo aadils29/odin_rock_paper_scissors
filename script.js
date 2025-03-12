a11ec5f12e065faad364dde66937640fc2fa7301
aadils <aadilsingh@outlook.com>
2025-03-11 21:07:48 -0400

    Make function for single round and adds to score

--- script.js
+++ script.js
@@ -3,15 +3,35 @@ function getComputerChoice() {
 
   switch (choice) {
     case 0:
-      return "rock";
+      return "ROCK";
     case 1:
-      return "paper";
+      return "PAPER";
     case 2:
-      return "scissors";
+      return "SCISSORS";
   }
 }
 
 function getHumanChoice() {
   const choice = prompt("Choose between Rock, Paper, or Scissors");
   return choice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice_upper = humanChoice.toUpperCase();
+
+  if (humanChoice_upper === computerChoice) console.log("It's a Tie!");
+  else if (
+    (humanChoice_upper === "ROCK" && computerChoice === "SCISSORS") ||
+    (humanChoice_upper === "PAPER" && computerChoice === "ROCK") ||
+    (humanChoice_upper === "SCISSORS" && computerChoice === "PAPER")
+  ) {
+    console.log(`You win! ${humanChoice_upper} beats ${computerChoice}!`);
+    humanScore++;
+  } else {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice_upper}!`);
+    computerScore++;
+  }
 }
