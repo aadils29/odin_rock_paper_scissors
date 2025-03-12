505b084cb8d38f9f19af2356feb5138cccb329e8
aadils <aadilsingh@outlook.com>
2025-03-11 21:34:10 -0400

    Make function to play game 5 times and show winner

--- script.js
+++ script.js
@@ -16,22 +16,45 @@ function getHumanChoice() {
   return choice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-  humanChoice_upper = humanChoice.toUpperCase();
+  //function for one round
+  function playRound(humanChoice, computerChoice) {
+    humanChoice_upper = humanChoice.toUpperCase();
 
-  if (humanChoice_upper === computerChoice) console.log("It's a Tie!");
-  else if (
-    (humanChoice_upper === "ROCK" && computerChoice === "SCISSORS") ||
-    (humanChoice_upper === "PAPER" && computerChoice === "ROCK") ||
-    (humanChoice_upper === "SCISSORS" && computerChoice === "PAPER")
-  ) {
-    console.log(`You win! ${humanChoice_upper} beats ${computerChoice}!`);
-    humanScore++;
-  } else {
-    console.log(`You lose! ${computerChoice} beats ${humanChoice_upper}!`);
-    computerScore++;
+    if (humanChoice_upper === computerChoice) console.log("It's a Tie!");
+    else if (
+      (humanChoice_upper === "ROCK" && computerChoice === "SCISSORS") ||
+      (humanChoice_upper === "PAPER" && computerChoice === "ROCK") ||
+      (humanChoice_upper === "SCISSORS" && computerChoice === "PAPER")
+    ) {
+      console.log(`You win! ${humanChoice_upper} beats ${computerChoice}!`);
+      humanScore++;
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice_upper}!`);
+      computerScore++;
+    }
   }
+
+  // play game for 5 rounds
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+
+  if (humanScore > computerScore)
+    console.log(
+      `You won! Your score: ${humanScore} Computer score: ${computerScore}`
+    );
+  else if (computerScore > humanScore)
+    console.log(
+      `You lost! Your score: ${humanScore} Computer score: ${computerScore}`
+    );
+  else
+    console.log(
+      `It's a tie! Your score: ${humanScore} Computer score: ${computerScore}`
+    );
 }
